f25709a342a1261d4900ce346244f34fa1cc5f2e
asm2212 <asmareadmasu0@gmail.com>
2025-05-29 20:41:36 +0300

    Add MongoDB service to docker-compose and configure database connection in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,13 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI!)
+.then(() => console.log("connected to mongodb"))
+.catch((err) => console.error(err));
 
 app.use(cors());
 app.use(helmet());
@@ -17,4 +23,3 @@ app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
 })
 
-dotenv.config();
